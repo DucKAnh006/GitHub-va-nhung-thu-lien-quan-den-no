72e8eaa9672cd93a026d4cc6fcbc52421a6ba52f
Mr.DuckAnh <trananh0935@gmail.com>
2026-03-02 22:02:05 +0700

    feat(practice) practice after learning
    -add text

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Nguyen Tran Duc Anh");
+Console.WriteLine("My name is Nguyen Tran Duc Anh. this is my exercise for GitHub");
